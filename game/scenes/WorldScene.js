c82c554290cb474c5e25b9f5aa97d7efa758c688
Yerd8roll-hue <Yerd8_roll@hotmail.com>
2026-07-29 05:42:20 -0500

    Update WorldScene.js

--- game/scenes/WorldScene.js
+++ game/scenes/WorldScene.js
@@ -6,18 +6,61 @@ class WorldScene extends Phaser.Scene {
 
     create() {
 
+        // Fondo
         this.cameras.main.setBackgroundColor("#1b263b");
 
-        this.add.text(150, 150, "VALLE DEL PRIMER CUARZO", {
+
+        // Nombre del lugar
+        this.add.text(150, 40, "VALLE DEL PRIMER CUARZO", {
             fontSize: "40px",
             color: "#00ffff"
         });
 
-        this.add.text(250, 250, "MUNDO CARGADO", {
-            fontSize: "30px",
-            color: "#ffffff"
+
+        // Jugador
+        this.player = this.add.rectangle(
+            400,
+            300,
+            50,
+            70,
+            0x00ff00
+        );
+
+
+        // Controles
+        this.keys = this.input.keyboard.createCursorKeys();
+
+
+        // Texto de misión
+        this.add.text(180, 520, "Explora el valle y encuentra el cuarzo 💎", {
+            fontSize: "25px",
+            color: "#ffff00"
         });
 
     }
-}
 
+
+    update() {
+
+        let speed = 4;
+
+
+        if (this.keys.left.isDown) {
+            this.player.x -= speed;
+        }
+
+        if (this.keys.right.isDown) {
+            this.player.x += speed;
+        }
+
+        if (this.keys.up.isDown) {
+            this.player.y -= speed;
+        }
+
+        if (this.keys.down.isDown) {
+            this.player.y += speed;
+        }
+
+    }
+
+}

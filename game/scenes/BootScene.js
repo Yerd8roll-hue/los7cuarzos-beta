9ac1218eabb57a9d22fa191fd5085ad134e72d06
Yerd8roll-hue <Yerd8_roll@hotmail.com>
2026-07-29 04:56:40 -0500

    Update BootScene.js

--- game/scenes/BootScene.js
+++ game/scenes/BootScene.js
@@ -1,1 +1,20 @@
+class BootScene extends Phaser.Scene {
 
+    constructor() {
+        super("BootScene");
+    }
+
+    create() {
+
+        this.add.text(150, 150, "LOS 7 CUARZOS", {
+            fontSize: "50px",
+            color: "#00ffff"
+        });
+
+        this.add.text(150, 230, "ESCENA INICIADA", {
+            fontSize: "30px",
+            color: "#ffffff"
+        });
+
+    }
+}

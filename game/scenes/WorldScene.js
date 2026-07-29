95efc1e0c3abaa6508c04144ac1f6a32592dfacc
Yerd8roll-hue <Yerd8_roll@hotmail.com>
2026-07-29 06:20:39 -0500

    Update WorldScene.js

--- game/scenes/WorldScene.js
+++ game/scenes/WorldScene.js
@@ -8,23 +8,30 @@ class WorldScene extends Phaser.Scene {
     create() {
 
         // Fondo del mundo
-        this.cameras.main.setBackgroundColor("#1b263b");
+        this.cameras.main.setBackgroundColor("#080818");
 
 
-        // Nombre del lugar
+        // Título
         this.add.text(150, 40, "VALLE DEL PRIMER CUARZO", {
             fontSize: "40px",
             color: "#00ffff"
         });
 
 
+
         // Contador de cuarzos
         this.cuarzos = 0;
 
-        this.textoCuarzos = this.add.text(20, 20, "💎 Cuarzos: 0/7", {
-            fontSize: "25px",
-            color: "#ffffff"
-        });
+        this.textoCuarzos = this.add.text(
+            20,
+            20,
+            "💎 Cuarzos: 0/7",
+            {
+                fontSize: "25px",
+                color: "#ffffff"
+            }
+        );
+
 
 
         // Jugador temporal
@@ -37,26 +44,31 @@ class WorldScene extends Phaser.Scene {
         );
 
 
-        // Controles
+        // Teclado
         this.keys = this.input.keyboard.createCursorKeys();
 
 
 
         // =========================
-        // PRIMER CUARZO MÁGICO
+        // PRIMER CUARZO LEGENDARIO
         // =========================
+
+
+        this.cuarzoEncontrado = false;
 
         this.alturaInicialCuarzo = 300;
 
 
+
         // Aura del cuarzo
-        this.cuarzoBrillo = this.add.circle(
+        this.cuarzoAura = this.add.circle(
             600,
             300,
-            45,
+            55,
             0xff00ff,
             0.25
         );
+
 
 
         // Cristal principal
@@ -68,7 +80,8 @@ class WorldScene extends Phaser.Scene {
         );
 
 
-        // Animación del cristal
+
+        // Pulso del cristal
         this.tweens.add({
 
             targets: this.cuarzo,
@@ -86,46 +99,81 @@ class WorldScene extends Phaser.Scene {
         });
 
 
-        // Animación del aura
+
+        // Pulso del aura
         this.tweens.add({
 
-            targets: this.cuarzoBrillo,
+            targets: this.cuarzoAura,
 
-            scale: 1.8,
+            scale: 2,
 
             alpha: 0.1,
 
-            duration: 1000,
+            duration: 1200,
 
             yoyo: true,
 
-            repeat: -1,
-
-            ease: "Sine.easeInOut"
+            repeat: -1
 
         });
 
 
 
-        this.add.text(540, 350, "💎 Cuarzo", {
-            fontSize: "20px",
+        // Partículas de energía
+        this.particulas = [];
+
+
+        for(let i = 0; i < 15; i++) {
+
+            let energia = this.add.circle(
+                600,
+                300,
+                4,
+                0xffffff
+            );
+
+
+            this.particulas.push(energia);
+
+        }
+
+
+
+        this.add.text(520, 350, "💎 Primer Cuarzo", {
+            fontSize: "22px",
             color: "#ffffff"
         });
 
 
 
-        // Mensaje de misión
-        this.add.text(180, 520, "Explora el valle y encuentra el primer cuarzo 💎", {
-            fontSize: "25px",
-            color: "#ffff00"
-        });
+        // Mensaje inferior
+        this.add.text(
+            180,
+            520,
+            "Encuentra los 7 Cuarzos legendarios",
+            {
+                fontSize: "25px",
+                color: "#ffff00"
+            }
+        );
 
 
 
-        // Estado del cuarzo
-        this.cuarzoEncontrado = false;
+        // Caja de historia
+        this.historia = this.add.text(
+            120,
+            170,
+            "",
+            {
+                fontSize: "25px",
+                color: "#ffffff",
+                backgroundColor: "#111122",
+                padding: 10
+            }
+        );
 
     }
+
 
 
 
@@ -136,30 +184,30 @@ class WorldScene extends Phaser.Scene {
 
 
 
-        // Movimiento del jugador
+        // Movimiento jugador
 
-        if (this.keys.left.isDown) {
+        if(this.keys.left.isDown) {
 
             this.player.x -= speed;
 
         }
 
 
-        if (this.keys.right.isDown) {
+        if(this.keys.right.isDown) {
 
             this.player.x += speed;
 
         }
 
 
-        if (this.keys.up.isDown) {
+        if(this.keys.up.isDown) {
 
             this.player.y -= speed;
 
         }
 
 
-        if (this.keys.down.isDown) {
+        if(this.keys.down.isDown) {
 
             this.player.y += speed;
 
@@ -167,9 +215,11 @@ class WorldScene extends Phaser.Scene {
 
 
 
-        // Movimiento flotante del cuarzo
 
-        if (this.cuarzo && !this.cuarzoEncontrado) {
+        // Movimiento mágico del cuarzo
+
+        if(this.cuarzo && !this.cuarzoEncontrado) {
+
 
             let movimiento =
             Math.sin(this.time.now / 300) * 10;
@@ -179,33 +229,50 @@ class WorldScene extends Phaser.Scene {
             this.alturaInicialCuarzo + movimiento;
 
 
-            this.cuarzoBrillo.y =
+            this.cuarzoAura.y =
             this.alturaInicialCuarzo + movimiento;
+
+
+
+            // Movimiento partículas
+
+            this.particulas.forEach((p, i) => {
+
+                let angulo =
+                this.time.now / 500 + i;
+
+
+                p.x =
+                600 + Math.cos(angulo) * 70;
+
+
+                p.y =
+                this.cuarzo.y + Math.sin(angulo) * 70;
+
+
+            });
 
         }
 
 
 
-        // Detectar recogida
 
-        if (!this.cuarzoEncontrado) {
+        // Recoger cuarzo
+
+        if(!this.cuarzoEncontrado) {
 
 
-            let distancia = Phaser.Math.Distance.Between(
-
+            let distancia =
+            Phaser.Math.Distance.Between(
                 this.player.x,
-
                 this.player.y,
-
                 this.cuarzo.x,
-
                 this.cuarzo.y
-
             );
 
 
 
-            if (distancia < 50) {
+            if(distancia < 50) {
 
 
                 this.cuarzoEncontrado = true;
@@ -213,11 +280,12 @@ class WorldScene extends Phaser.Scene {
 
                 this.cuarzo.destroy();
 
-                this.cuarzoBrillo.destroy();
+                this.cuarzoAura.destroy();
 
 
 
                 this.cuarzos = 1;
+
 
 
                 this.textoCuarzos.setText(
@@ -226,14 +294,12 @@ class WorldScene extends Phaser.Scene {
 
 
 
-                this.add.text(150, 150,
-                    "¡HAS ENCONTRADO EL PRIMER CUARZO!",
-                    {
-                        fontSize: "30px",
-                        color: "#ffff00"
-                    }
+                this.historia.setText(
+                    "PRIMER CUARZO DESPERTADO\n\n" +
+                    "El cristal antiguo reconoce\n" +
+                    "al nuevo portador.\n\n" +
+                    "Poder obtenido:\n✨ Energía del alma"
                 );
-
 
             }
 
@@ -243,3 +309,5 @@ class WorldScene extends Phaser.Scene {
     }
 
 }
+
+    

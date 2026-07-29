bc895410e20ef6985a2970cdcc438080bd7a5e25
Yerd8roll-hue <Yerd8_roll@hotmail.com>
2026-07-29 04:21:23 -0500

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,9 +1,21 @@
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: "#111111",
-    scene: [BootScene]
-};
+class BootScene extends Phaser.Scene {
+    constructor() {
+        super("BootScene");
+    }
 
-const game = new Phaser.Game(config);
+    preload() {
+
+    }
+
+    create() {
+        this.add.text(220, 180, "LOS 7 CUARZOS", {
+            fontSize: "40px",
+            color: "#00ffff"
+        });
+
+        this.add.text(220, 240, "BootScene cargada correctamente", {
+            fontSize: "22px",
+            color: "#ffffff"
+        });
+    }
+}

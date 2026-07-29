19b2eac5d596693d45abe7b1e8fd55e5409138c6
Yerd8roll-hue <Yerd8_roll@hotmail.com>
2026-07-29 05:07:36 -0500

    Update BootScene.js

--- game/scenes/BootScene.js
+++ game/scenes/BootScene.js
@@ -5,9 +5,58 @@ class BootScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.text(200, 200, "LOS 7 CUARZOS", {
-            fontSize: "40px",
+
+        // Título
+        this.add.text(180, 80, "LOS 7 CUARZOS", {
+            fontSize: "50px",
             color: "#00ffff"
         });
+
+        // Botón Nueva Partida
+        let start = this.add.text(260, 220, "▶ NUEVA PARTIDA", {
+            fontSize: "30px",
+            color: "#ffffff"
+        });
+
+        start.setInteractive();
+
+        start.on("pointerover", () => {
+            start.setColor("#00ffff");
+        });
+
+        start.on("pointerout", () => {
+            start.setColor("#ffffff");
+        });
+
+        start.on("pointerdown", () => {
+            console.log("Iniciando aventura...");
+        });
+
+
+        // Historia
+        let historia = this.add.text(290, 300, "📖 HISTORIA", {
+            fontSize: "30px",
+            color: "#ffffff"
+        });
+
+        historia.setInteractive();
+
+        historia.on("pointerdown", () => {
+            console.log("Abriendo historia...");
+        });
+
+
+        // Opciones
+        let opciones = this.add.text(300, 380, "⚙ OPCIONES", {
+            fontSize: "30px",
+            color: "#ffffff"
+        });
+
+        opciones.setInteractive();
+
+        opciones.on("pointerdown", () => {
+            console.log("Opciones...");
+        });
+
     }
 }

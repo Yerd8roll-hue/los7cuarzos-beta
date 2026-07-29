fbeaff6a36fe53acd2ac9ccaafb040b2c995d6e7
Yerd8roll-hue <Yerd8_roll@hotmail.com>
2026-07-29 05:10:23 -0500

    Update BootScene.js

--- game/scenes/BootScene.js
+++ game/scenes/BootScene.js
@@ -6,15 +6,26 @@ class BootScene extends Phaser.Scene {
 
     create() {
 
+        // Fondo
+        this.cameras.main.setBackgroundColor("#050510");
+
+
         // Título
-        this.add.text(180, 80, "LOS 7 CUARZOS", {
-            fontSize: "50px",
-            color: "#00ffff"
+        this.add.text(150, 80, "LOS 7 CUARZOS", {
+            fontSize: "55px",
+            color: "#00ffff",
+            fontStyle: "bold"
         });
 
-        // Botón Nueva Partida
-        let start = this.add.text(260, 220, "▶ NUEVA PARTIDA", {
-            fontSize: "30px",
+
+        // Caja del menú
+        this.add.rectangle(400, 330, 420, 300, 0x111122)
+            .setStrokeStyle(3, 0x00ffff);
+
+
+        // Botón Nueva partida
+        let start = this.add.text(270, 220, "▶ NUEVA PARTIDA", {
+            fontSize: "32px",
             color: "#ffffff"
         });
 
@@ -28,35 +39,23 @@ class BootScene extends Phaser.Scene {
             start.setColor("#ffffff");
         });
 
-        start.on("pointerdown", () => {
-            console.log("Iniciando aventura...");
-        });
-
 
         // Historia
-        let historia = this.add.text(290, 300, "📖 HISTORIA", {
-            fontSize: "30px",
+        let historia = this.add.text(310, 300, "HISTORIA", {
+            fontSize: "32px",
             color: "#ffffff"
         });
 
         historia.setInteractive();
 
-        historia.on("pointerdown", () => {
-            console.log("Abriendo historia...");
-        });
-
 
         // Opciones
-        let opciones = this.add.text(300, 380, "⚙ OPCIONES", {
-            fontSize: "30px",
+        let opciones = this.add.text(310, 380, "OPCIONES", {
+            fontSize: "32px",
             color: "#ffffff"
         });
 
         opciones.setInteractive();
-
-        opciones.on("pointerdown", () => {
-            console.log("Opciones...");
-        });
 
     }
 }

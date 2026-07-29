3b40e1add8a14ca15163432749c35f45a48a8333
Yerd8roll-hue <Yerd8_roll@hotmail.com>
2026-07-29 05:39:25 -0500

    Update main.js

--- game/main.js
+++ game/main.js
@@ -1,13 +1,9 @@
-const game = new Phaser.Game({
-
+const config = {
     type: Phaser.AUTO,
-
     width: 800,
-
     height: 600,
-
     backgroundColor: "#111111",
-
     scene: [BootScene, WorldScene]
+};
 
-});
+const game = new Phaser.Game(config);

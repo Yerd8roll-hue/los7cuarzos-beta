6d0032aec126676b97ea4893a11b7bfd9d2306a7
Yerd8roll-hue <Yerd8_roll@hotmail.com>
2026-07-29 22:31:25 -0500

    Update BootScene.js

--- game/scenes/BootScene.js
+++ game/scenes/BootScene.js
@@ -1,3 +1,4 @@
+
 console.log("ESTE ES EL BOOTSCENE NUEVO");
 
 class BootScene extends Phaser.Scene {
@@ -6,70 +7,97 @@ class BootScene extends Phaser.Scene {
         super("BootScene");
     }
 
+    preload(){
+
+        // PERSONAJES
+        this.load.image(
+            "kael",
+            "imagenes/personajes/kael_vortex.png"
+        );
+
+        // CUARZOS
+        this.load.image(
+            "alma",
+            "imagenes/cuarzos/alma.png"
+        );
+
+        // FONDOS
+        this.load.image(
+            "neoTerra",
+            "imagenes/fondos/neo_terra.png"
+        );
+
+    }
+
     create() {
 
-        // Fondo
         this.cameras.main.setBackgroundColor("#050510");
 
-
-        // Título
-        this.add.text(150, 80, "LOS 7 CUARZOS", {
-            fontSize: "55px",
-            color: "#00ffff",
-            fontStyle: "bold"
+        this.add.text(150,80,"LOS 7 CUARZOS",{
+            fontSize:"55px",
+            color:"#00ffff",
+            fontStyle:"bold"
         });
 
+        this.add.rectangle(400,330,420,300,0x111122)
+            .setStrokeStyle(3,0x00ffff);
 
-        // Caja del menú
-        this.add.rectangle(400, 330, 420, 300, 0x111122)
-            .setStrokeStyle(3, 0x00ffff);
-
-
-        // Botón Nueva Partida
-        let start = this.add.text(270, 220, "▶ NUEVA PARTIDA", {
-            fontSize: "32px",
-            color: "#ffffff"
-        });
+        let start=this.add.text(
+            270,
+            220,
+            "▶ NUEVA PARTIDA",
+            {
+                fontSize:"32px",
+                color:"#ffffff"
+            }
+        );
 
         start.setInteractive();
 
-        start.on("pointerover", () => {
+        start.on("pointerover",()=>{
             start.setColor("#00ffff");
         });
 
-        start.on("pointerout", () => {
+        start.on("pointerout",()=>{
             start.setColor("#ffffff");
         });
 
-        start.on("pointerdown", () => {
+        start.on("pointerdown",()=>{
             this.scene.start("WorldScene");
         });
 
-
-        // Historia
-        let historia = this.add.text(310, 300, "HISTORIA", {
-            fontSize: "32px",
-            color: "#ffffff"
-        });
+        let historia=this.add.text(
+            310,
+            300,
+            "HISTORIA",
+            {
+                fontSize:"32px",
+                color:"#ffffff"
+            }
+        );
 
         historia.setInteractive();
 
-        historia.on("pointerdown", () => {
+        historia.on("pointerdown",()=>{
             console.log("Historia");
         });
 
-
-        // Opciones
-        let opciones = this.add.text(310, 380, "OPCIONES", {
-            fontSize: "32px",
-            color: "#ffffff"
-        });
+        let opciones=this.add.text(
+            310,
+            380,
+            "OPCIONES",
+            {
+                fontSize:"32px",
+                color:"#ffffff"
+            }
+        );
 
         opciones.setInteractive();
 
-        opciones.on("pointerdown", () => {
+        opciones.on("pointerdown",()=>{
             console.log("Opciones");
         });
 
     }
+
 }

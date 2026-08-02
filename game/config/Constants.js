177ac11a4b05f5818e03ccf65c5c0be9fb1bc4b4
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-02 18:50:27 -0500

    Update Constants.js

--- game/config/Constants.js
+++ game/config/Constants.js
@@ -1,1 +1,25 @@
+// config/Constants.js
 
+const Constants = {
+
+    GAME_NAME: "Los 7 Cuarzos",
+
+    VERSION: "Beta 2.0",
+
+    MAX_HEALTH: 100,
+
+    PLAYER_SPEED: 250,
+
+    DASH_SPEED: 500,
+
+    JUMP_FORCE: -520,
+
+    DOUBLE_JUMP_FORCE: -470,
+
+    GRAVITY: 900,
+
+    TOTAL_QUARTZ: 7
+
+};
+
+export default Constants;

3bc49ef1958ec6b3fb824f892e44f3cd602e5274
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-02 18:53:28 -0500

    Update PreloadScene.js

--- game/scenes/PreloadScene.js
+++ game/scenes/PreloadScene.js
@@ -1,1 +1,23 @@
+// game/scenes/PreloadScene.js
 
+export default class PreloadScene extends Phaser.Scene {
+
+    constructor() {
+        super("PreloadScene");
+    }
+
+    preload() {
+
+        // Aquí cargaremos imágenes, sonidos y sprites.
+
+        console.log("Cargando recursos...");
+
+    }
+
+    create() {
+
+        this.scene.start("MenuScene");
+
+    }
+
+}

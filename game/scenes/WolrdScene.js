225e41dbe0814c62a471aab72dfc8f0b5e0ba2fc
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-02 18:54:31 -0500

    Update WolrdScene.js

--- game/scenes/WolrdScene.js
+++ game/scenes/WolrdScene.js
@@ -1,1 +1,18 @@
+// game/scenes/WorldScene.js
 
+export default class WorldScene extends Phaser.Scene {
+
+    constructor() {
+        super("WorldScene");
+    }
+
+    create() {
+
+        this.add.text(640, 360, "Valle del Cuarzo del Alma", {
+            fontSize: "36px",
+            color: "#ffffff"
+        }).setOrigin(0.5);
+
+    }
+
+}

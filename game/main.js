3f5be8f7d512825f46561ebd5dc06abaf2c1a2f2
Yerd8roll-hue <Yerd8_roll@hotmail.com>
2026-08-02 18:07:05 -0500

    Delete game/main.js

--- game/main.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: "#111111",
-    scene: [BootScene, WorldScene]
-};
-
-const game = new Phaser.Game(config);

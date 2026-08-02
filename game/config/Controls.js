3e635b9df79857681a475422f51f224586d8f229
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-02 18:49:55 -0500

    Update Controls.js

--- game/config/Controls.js
+++ game/config/Controls.js
@@ -1,1 +1,29 @@
+// config/Controls.js
 
+const Controls = {
+
+    LEFT: [
+        "LEFT",
+        "A"
+    ],
+
+    RIGHT: [
+        "RIGHT",
+        "D"
+    ],
+
+    JUMP: [
+        "UP",
+        "W",
+        "SPACE"
+    ],
+
+    ATTACK: "J",
+
+    DASH: "SHIFT",
+
+    PAUSE: "ESC"
+
+};
+
+export default Controls;

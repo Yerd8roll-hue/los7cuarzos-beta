cfe013239821bc68dbfeef06dc7ffa3bc6532ef4
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-02 18:52:56 -0500

    Update BootScene.js

--- game/scenes/BootScene.js
+++ game/scenes/BootScene.js
@@ -1,1 +1,17 @@
+// game/scenes/BootScene.js
 
+export default class BootScene extends Phaser.Scene {
+
+    constructor() {
+        super("BootScene");
+    }
+
+    preload() {
+        console.log("Iniciando juego...");
+    }
+
+    create() {
+        this.scene.start("PreloadScene");
+    }
+
+}

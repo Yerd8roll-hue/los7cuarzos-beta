5330c1593013abab849f28580876d8a72159e671
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-03 18:58:53 -0500

    Update PreloadScene.js

--- game/scenes/PreloadScene.js
+++ game/scenes/PreloadScene.js
@@ -1,80 +1,39 @@
-import { createKaelAnimations } from "../animations/KaelAnimations.js";
+// game/scenes/PreloadScene.js
 
-export default class WorldScene extends Phaser.Scene {
+export default class PreloadScene extends Phaser.Scene {
 
     constructor() {
-        super("WorldScene");
+        super("PreloadScene");
+    }
+
+    preload() {
+
+        // Texto de carga
+        this.add.text(640, 360, "Cargando...", {
+            fontSize: "32px",
+            color: "#ffffff"
+        }).setOrigin(0.5);
+
+        // ===== KAEL =====
+        this.load.spritesheet("kael", "assets/personajes/Kael/spritesheet.png", {
+            frameWidth: 64,
+            frameHeight: 64
+        });
+
+        // ===== FONDOS =====
+        this.load.image("sky", "assets/fondos/sky.png");
+        this.load.image("cityBack", "assets/fondos/cityBack.png");
+        this.load.image("cityFront", "assets/fondos/cityFront.png");
+        this.load.image("cables", "assets/fondos/cables.png");
+
+        // ===== SUELO =====
+        this.load.image("floor", "assets/floor.png");
+
     }
 
     create() {
 
-        // Fondo
-        this.cameras.main.setBackgroundColor("#1a1a2e");
-
-        // Mundo
-        this.physics.world.setBounds(0, 0, 3000, 720);
-
-        // Título
-        this.add.text(640, 40, "Valle del Cuarzo del Alma", {
-            fontSize: "32px",
-            color: "#00ffff"
-        }).setOrigin(0.5).setScrollFactor(0);
-
-        // Suelo
-        this.ground = this.add.rectangle(1500, 690, 3000, 60, 0x444444);
-        this.physics.add.existing(this.ground, true);
-
-        // Animaciones
-        createKaelAnimations(this);
-
-        // Kael
-        this.kael = this.physics.add.sprite(200, 500, "kael");
-
-        this.kael.setCollideWorldBounds(true);
-        this.kael.setBounce(0);
-
-        this.physics.add.collider(this.kael, this.ground);
-
-        // Cámara
-        this.cameras.main.startFollow(this.kael);
-        this.cameras.main.setBounds(0, 0, 3000, 720);
-
-        // Controles
-        this.cursors = this.input.keyboard.createCursorKeys();
-
-        this.keys = this.input.keyboard.addKeys({
-            A: Phaser.Input.Keyboard.KeyCodes.A,
-            D: Phaser.Input.Keyboard.KeyCodes.D,
-            SPACE: Phaser.Input.Keyboard.KeyCodes.SPACE
-        });
-
-    }
-
-    update() {
-
-        if (this.keys.A.isDown || this.cursors.left.isDown) {
-
-            this.kael.setVelocityX(-180);
-            this.kael.setFlipX(true);
-
-        }
-        else if (this.keys.D.isDown || this.cursors.right.isDown) {
-
-            this.kael.setVelocityX(180);
-            this.kael.setFlipX(false);
-
-        }
-        else {
-
-            this.kael.setVelocityX(0);
-
-        }
-
-        if ((this.keys.SPACE.isDown || this.cursors.up.isDown) && this.kael.body.blocked.down) {
-
-            this.kael.setVelocityY(-500);
-
-        }
+        this.scene.start("MenuScene");
 
     }
 

d5c68c6a3317255c0f01daf78392a636b47437a8
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-02 23:43:03 -0500

    Update PreloadScene.js

--- game/scenes/PreloadScene.js
+++ game/scenes/PreloadScene.js
@@ -1,34 +1,8 @@
-// game/scenes/PreloadScene.js
-
-export default class PreloadScene extends Phaser.Scene {
-
-    constructor() {
-        super("PreloadScene");
+this.load.spritesheet(
+    "kael",
+    "assets/personajes/Kael/spritesheet.png",
+    {
+        frameWidth: 128,
+        frameHeight: 128
     }
-
-    preload() {
-
-       
-
-    this.load.spritesheet(
-        "kael",
-        "assets/personajes/Kael/spritesheet.png",
-        {
-            frameWidth: 128,
-            frameHeight: 128
-        }
-    );
-
-} 
-
-        console.log("Cargando recursos...");
-
-    }
-
-    create() {
-
-        this.scene.start("MenuScene");
-
-    }
-
-}
+);

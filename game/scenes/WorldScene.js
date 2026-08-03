04dbd833dab607e6c2c63158d5f871b06db7c5c9
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-03 13:19:22 -0500

    Update WorldScene.js

--- game/scenes/WorldScene.js
+++ game/scenes/WorldScene.js
@@ -6,20 +6,18 @@ export default class WorldScene extends Phaser.Scene {
 
     create() {
 
-        // Fondo
         this.cameras.main.setBackgroundColor("#1a1a2e");
 
-        // Título
         this.add.text(640, 40, "Valle del Cuarzo del Alma", {
             fontSize: "32px",
             color: "#00ffff"
         }).setOrigin(0.5);
 
-        // Mostrar la imagen completa
-        this.kael = this.add.image(640, 360, "kael");
+        console.log("¿Existe kael?", this.textures.exists("kael"));
 
-        // Reducir el tamaño para que se vea completa
-        this.kael.setScale(0.35);
+        const imagen = this.add.image(640, 360, "kael");
+
+        imagen.setScale(0.35);
 
     }
 

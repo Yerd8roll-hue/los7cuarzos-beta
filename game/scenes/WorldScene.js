77bcd97a1460c5dde7f59d52cc7237f9e320294d
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-03 13:13:52 -0500

    Update WorldScene.js

--- game/scenes/WorldScene.js
+++ game/scenes/WorldScene.js
@@ -1,7 +1,3 @@
-// game/scenes/WorldScene.js
-
-import { createKaelAnimations } from "../animations/KaelAnimations.js";
-
 export default class WorldScene extends Phaser.Scene {
 
     constructor() {
@@ -19,31 +15,15 @@ export default class WorldScene extends Phaser.Scene {
             color: "#00ffff"
         }).setOrigin(0.5);
 
-        // Crear animaciones
-        createKaelAnimations(this);
+        // Mostrar la imagen completa
+        this.kael = this.add.image(640, 360, "kael");
 
-        // Crear Kael
-        this.kael = this.physics.add.sprite(300, 500, "kael");
-
-        this.kael.setCollideWorldBounds(true);
-
-        // Animación inicial
-        this.kael.play("kael-idle");
-
-        // Controles
-        this.cursors = this.input.keyboard.createCursorKeys();
-
-        this.keys = this.input.keyboard.addKeys({
-            A: Phaser.Input.Keyboard.KeyCodes.A,
-            D: Phaser.Input.Keyboard.KeyCodes.D,
-            SPACE: Phaser.Input.Keyboard.KeyCodes.SPACE
-        });
+        // Reducir el tamaño para que se vea completa
+        this.kael.setScale(0.35);
 
     }
 
     update() {
-
-        // El movimiento lo agregaremos en el siguiente paso.
 
     }
 

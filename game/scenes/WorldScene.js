5b2b11fea64931f7e0754b6a162cf458b4a3fa40
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-02 23:38:23 -0500

    Update WorldScene.js

--- game/scenes/WorldScene.js
+++ game/scenes/WorldScene.js
@@ -8,11 +8,28 @@ export default class WorldScene extends Phaser.Scene {
 
     create() {
 
-        this.add.text(640, 360, "Valle del Cuarzo del Alma", {
-            fontSize: "36px",
-            color: "#ffffff"
+        // Fondo
+        this.cameras.main.setBackgroundColor("#1a1a2e");
+
+        // Título
+        this.add.text(640, 40, "Valle del Cuarzo del Alma", {
+            fontSize: "32px",
+            color: "#00ffff"
         }).setOrigin(0.5);
+
+        // Kael
+        this.kael = this.physics.add.sprite(300, 500, "kael");
+
+        this.kael.setFrame(0);
+
+        this.kael.setScale(1);
+
+        this.kael.setCollideWorldBounds(true);
 
     }
 
-}
+    update() {
+
+    }
+
+} 

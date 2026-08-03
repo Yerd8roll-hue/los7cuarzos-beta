8066ea6cbfbbb97f8c7f9487c9ba5efbccedbdb9
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-03 13:33:14 -0500

    Update KaelAnimations.js

--- game/animations/KaelAnimations.js
+++ game/animations/KaelAnimations.js
@@ -1,53 +1,7 @@
 export function createKaelAnimations(scene) {
 
-    scene.anims.create({
-        key: "kael-idle",
-        frames: scene.anims.generateFrameNumbers("kael", {
-            start: 0,
-            end: 5
-        }),
-        frameRate: 8,
-        repeat: -1
-    });
-
-    scene.anims.create({
-        key: "kael-walk",
-        frames: scene.anims.generateFrameNumbers("kael", {
-            start: 6,
-            end: 13
-        }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    scene.anims.create({
-        key: "kael-run",
-        frames: scene.anims.generateFrameNumbers("kael", {
-            start: 14,
-            end: 21
-        }),
-        frameRate: 14,
-        repeat: -1
-    });
-
-    scene.anims.create({
-        key: "kael-jump",
-        frames: scene.anims.generateFrameNumbers("kael", {
-            start: 22,
-            end: 25
-        }),
-        frameRate: 10,
-        repeat: 0
-    });
-
-    scene.anims.create({
-        key: "kael-fall",
-        frames: scene.anims.generateFrameNumbers("kael", {
-            start: 26,
-            end: 29
-        }),
-        frameRate: 10,
-        repeat: -1
-    });
+    if (!scene.textures.exists("kael")) {
+        return;
+    }
 
 }

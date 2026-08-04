5decbf6e3cc21d2d18ece339d24567195b5d6e27
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-03 20:01:18 -0500

    Update GameConfig.js

--- game/config/GameConfig.js
+++ game/config/GameConfig.js
@@ -1,3 +1,5 @@
+import BootScene from "../scenes/BootScene.js";
+import MenuScene from "../scenes/MenuScene.js";
 import PreloadScene from "../scenes/PreloadScene.js";
 import WorldScene from "../scenes/WorldScene.js";
 
@@ -9,25 +11,23 @@ const config = {
     width: 800,
     height: 600,
 
-
     physics: {
 
         default: "arcade",
 
         arcade: {
-
             gravity: {
                 y: 1000
             },
-
             debug: false
-
         }
 
     },
 
 
     scene: [
+        BootScene,
+        MenuScene,
         PreloadScene,
         WorldScene
     ]

c457c35e011df0cbeec86f50e0e2cba859531509
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-03 19:17:18 -0500

    Update PreloadScene.js

--- game/scenes/PreloadScene.js
+++ game/scenes/PreloadScene.js
@@ -1,40 +1,28 @@
-// game/scenes/PreloadScene.js
+//=========================
+// VALLE DEL ALMA
+//=========================
 
-export default class PreloadScene extends Phaser.Scene {
+this.load.image(
+    "sky",
+    "assets/valle del_alma/sky.png"
+);
 
-    constructor() {
-        super("PreloadScene");
-    }
+this.load.image(
+    "cityBack",
+    "assets/valle del_alma/city_back.png"
+);
 
-    preload() {
+this.load.image(
+    "cityFront",
+    "assets/valle del_alma/city_front.png"
+);
 
-        // Texto de carga
-        this.add.text(640, 360, "Cargando...", {
-            fontSize: "32px",
-            color: "#ffffff"
-        }).setOrigin(0.5);
+this.load.image(
+    "cables",
+    "assets/valle del_alma/cables.png"
+);
 
-        // ===== KAEL =====
-        this.load.spritesheet("kael", "assets/personajes/Kael/spritesheet.png", {
-            frameWidth: 64,
-            frameHeight: 64
-        });
-
-        // ===== FONDOS =====
-        this.load.image("sky", "assets/fondos/sky.png");
-        this.load.image("cityBack", "assets/fondos/cityBack.png");
-        this.load.image("cityFront", "assets/fondos/cityFront.png");
-        this.load.image("cables", "assets/fondos/cables.png");
-
-        // ===== SUELO =====
-        this.load.image("floor", "assets/floor.png");
-
-    }
-
-    create() {
-
-        this.scene.start("MenuScene");
-
-    }
-
-}
+this.load.image(
+    "floor",
+    "assets/valle del_alma/floor.png"
+);

32561103c196853a6025985e3c3ab1a14c9eb524
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-03 19:46:14 -0500

    Update GameConfig.js

--- game/config/GameConfig.js
+++ game/config/GameConfig.js
@@ -1,47 +1,41 @@
-// config/GameConfig.js
+import PreloadScene from "./scenes/PreloadScene.js";
+import WorldScene from "./scenes/WorldScene.js";
 
-import BootScene from "../scenes/BootScene.js";
-import PreloadScene from "../scenes/PreloadScene.js";
-import MenuScene from "../scenes/MenuScene.js";
-import WorldScene from "../scenes/WorldScene.js";
-import PauseScene from "../scenes/PauseScene.js";
-import UIScene from "../scenes/UIScene.js";
 
-const GameConfig = {
+const config = {
+
     type: Phaser.AUTO,
 
-    parent: "game-container",
 
-    width: 1280,
-    height: 720,
+    width: 800,
+    height: 600,
 
-    backgroundColor: "#0d0d0d",
-
-    pixelArt: false,
 
     physics: {
+
         default: "arcade",
+
         arcade: {
+
             gravity: {
-                y: 900
+                y: 1000
             },
+
             debug: false
+
         }
+
     },
 
-    scale: {
-        mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
 
     scene: [
-        BootScene,
+
         PreloadScene,
-        MenuScene,
-        WorldScene,
-        PauseScene,
-        UIScene
+        WorldScene
+
     ]
+
 };
 
-export default GameConfig;
+
+export default config;

92c5cb5a6e015a0896209e84850197bfcfa05d0e
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-05 16:33:40 -0500

    Update WorldScene.js

--- game/scenes/WorldScene.js
+++ game/scenes/WorldScene.js
@@ -1,336 +1,76 @@
-import { createKaelAnimations } from "../animations/KaelAnimations.js";
+//=========================================
+// FONDO VALLE DEL ALMA
+//=========================================
 
 
-export default class WorldScene extends Phaser.Scene {
+this.sky = this.add.tileSprite(
+    0,
+    0,
+    1280,
+    720,
+    "sky"
+)
+.setOrigin(0)
+.setScrollFactor(0);
 
 
-    constructor() {
 
-        super("WorldScene");
+this.cityBack = this.add.tileSprite(
+    0,
+    0,
+    1280,
+    720,
+    "cityBack"
+)
+.setOrigin(0)
+.setScrollFactor(0);
 
-    }
 
 
+this.cityFront = this.add.tileSprite(
+    0,
+    0,
+    1280,
+    720,
+    "cityFront"
+)
+.setOrigin(0)
+.setScrollFactor(0);
 
-    create() {
 
 
-        //=========================================
-        // COLOR DE FONDO
-        //=========================================
+this.cables = this.add.tileSprite(
+    0,
+    0,
+    1280,
+    720,
+    "cables"
+)
+.setOrigin(0)
+.setScrollFactor(0);
 
-        this.cameras.main.setBackgroundColor("#05070d");
 
 
+//=========================================
+// PISO DEL VALLE
+//=========================================
 
-        //=========================================
-        // MUNDO
-        //=========================================
 
-        this.physics.world.setBounds(
-            0,
-            0,
-            3000,
-            720
-        );
+this.floor = this.add.tileSprite(
+    0,
+    630,
+    3000,
+    100,
+    "floor"
+)
+.setOrigin(0);
 
 
 
-        //=========================================
-        // FONDO PARALLAX
-        //=========================================
+this.physics.add.existing(
+    this.floor,
+    true
+);
 
 
-        this.sky = this.add.tileSprite(
-            0,
-            0,
-            1280,
-            720,
-            "sky"
-        )
-        .setOrigin(0)
-        .setScrollFactor(0);
-
-
-
-        this.cityBack = this.add.tileSprite(
-            0,
-            0,
-            1280,
-            720,
-            "cityBack"
-        )
-        .setOrigin(0)
-        .setScrollFactor(0);
-
-
-
-        this.cityFront = this.add.tileSprite(
-            0,
-            0,
-            1280,
-            720,
-            "cityFront"
-        )
-        .setOrigin(0)
-        .setScrollFactor(0);
-
-
-
-        this.cables = this.add.tileSprite(
-            0,
-            0,
-            1280,
-            720,
-            "cables"
-        )
-        .setOrigin(0)
-        .setScrollFactor(0);
-
-
-
-        //=========================================
-        // PISO VISUAL DEL VALLE
-        //=========================================
-
-        this.floorImage = this.add.tileSprite(
-            0,
-            630,
-            3000,
-            100,
-            "floor"
-        )
-        .setOrigin(0);
-
-
-
-        //=========================================
-        // TITULO DEL NIVEL
-        //=========================================
-
-        this.add.text(
-            640,
-            50,
-            "VALLE DEL CUARZO\nDEL ALMA",
-            {
-
-                fontSize: "32px",
-                color: "#00ffff",
-                fontStyle: "bold",
-                align: "center"
-
-            }
-        )
-        .setOrigin(0.5)
-        .setScrollFactor(0);
-
-
-
-        //=========================================
-        // SUELO FISICO (NO TOCAR)
-        //=========================================
-
-        this.ground = this.add.rectangle(
-            1500,
-            690,
-            3000,
-            60,
-            0x353535
-        );
-
-
-        this.physics.add.existing(
-            this.ground,
-            true
-        );
-
-
-
-        //=========================================
-        // ANIMACIONES KAEL
-        //=========================================
-
-        createKaelAnimations(this);
-
-
-
-        //=========================================
-        // KAEL
-        //=========================================
-
-        this.kael = this.physics.add.sprite(
-            200,
-            500,
-            "kael"
-        );
-
-
-        this.kael.setBounce(0);
-
-        this.kael.setCollideWorldBounds(true);
-
-
-
-        this.physics.add.collider(
-            this.kael,
-            this.ground
-        );
-
-
-
-        //=========================================
-        // CAMARA
-        //=========================================
-
-        this.cameras.main.startFollow(
-            this.kael,
-            true
-        );
-
-
-        this.cameras.main.setBounds(
-            0,
-            0,
-            3000,
-            720
-        );
-
-
-
-        //=========================================
-        // CONTROLES
-        //=========================================
-
-        this.cursors =
-            this.input.keyboard.createCursorKeys();
-
-
-
-        this.keys =
-            this.input.keyboard.addKeys({
-
-                A:
-                Phaser.Input.Keyboard.KeyCodes.A,
-
-
-                D:
-                Phaser.Input.Keyboard.KeyCodes.D,
-
-
-                SPACE:
-                Phaser.Input.Keyboard.KeyCodes.SPACE
-
-            });
-
-
-    }
-
-
-
-
-
-    update() {
-
-
-
-        //=========================================
-        // PARALLAX
-        //=========================================
-
-
-        this.sky.tilePositionX =
-            this.cameras.main.scrollX * 0.05;
-
-
-
-        this.cityBack.tilePositionX =
-            this.cameras.main.scrollX * 0.15;
-
-
-
-        this.cityFront.tilePositionX =
-            this.cameras.main.scrollX * 0.30;
-
-
-
-        this.cables.tilePositionX =
-            this.cameras.main.scrollX * 0.45;
-
-
-
-        //=========================================
-        // MOVIMIENTO KAEL
-        //=========================================
-
-
-        if (
-
-            this.keys.A.isDown ||
-            this.cursors.left.isDown
-
-        ) {
-
-
-            this.kael.setVelocityX(-180);
-
-            this.kael.setFlipX(true);
-
-
-        }
-
-        else if (
-
-            this.keys.D.isDown ||
-            this.cursors.right.isDown
-
-        ) {
-
-
-            this.kael.setVelocityX(180);
-
-            this.kael.setFlipX(false);
-
-
-        }
-
-        else {
-
-
-            this.kael.setVelocityX(0);
-
-
-        }
-
-
-
-        //=========================================
-        // SALTO
-        //=========================================
-
-
-        if (
-
-            (
-                this.keys.SPACE.isDown ||
-                this.cursors.up.isDown
-            )
-
-            &&
-
-            this.kael.body.blocked.down
-
-        ) {
-
-
-            this.kael.setVelocityY(-500);
-
-
-        }
-
-
-    }
-
-
-}
-
-
+    

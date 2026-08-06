db9743db11864d0c42141a296b665e40784a0be5
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-06 00:04:01 -0500

    Update WorldScene.js

--- game/scenes/WorldScene.js
+++ game/scenes/WorldScene.js
@@ -1,25 +1,19 @@
 import { createKaelAnimations } from "../animations/KaelAnimations.js";
 
+const WORLD_WIDTH = 3000;
+const WORLD_HEIGHT = 941;
+
+const FLOOR_Y = 900;
 
 export default class WorldScene extends Phaser.Scene {
 
-
     constructor() {
+
         super("WorldScene");
+
     }
 
-
-
     create() {
-
-
-        //=========================================
-        // COLOR DE FONDO
-        //=========================================
-
-        this.cameras.main.setBackgroundColor("#05070d");
-
-
 
         //=========================================
         // MUNDO
@@ -28,18 +22,22 @@ export default class WorldScene extends Phaser.Scene {
         this.physics.world.setBounds(
             0,
             0,
-            3000,
-            720
+            WORLD_WIDTH,
+            WORLD_HEIGHT
         );
 
+        this.cameras.main.setBounds(
+            0,
+            0,
+            WORLD_WIDTH,
+            WORLD_HEIGHT
+        );
 
+        this.cameras.main.setBackgroundColor("#05070d");
 
         //=========================================
-        // CAPAS DEL VALLE DEL ALMA
+        // FONDO
         //=========================================
-
-
-        // CIELO
 
         this.sky = this.add.image(
             0,
@@ -47,12 +45,7 @@ export default class WorldScene extends Phaser.Scene {
             "sky"
         )
         .setOrigin(0)
-        .setDepth(0)
-        .setScrollFactor(0);
-
-
-
-        // CIUDAD LEJANA
+        .setDepth(0);
 
         this.cityBack = this.add.image(
             0,
@@ -60,12 +53,7 @@ export default class WorldScene extends Phaser.Scene {
             "cityBack"
         )
         .setOrigin(0)
-        .setDepth(1)
-        .setScrollFactor(0);
-
-
-
-        // CIUDAD CERCANA
+        .setDepth(1);
 
         this.cityFront = this.add.image(
             0,
@@ -73,12 +61,15 @@ export default class WorldScene extends Phaser.Scene {
             "cityFront"
         )
         .setOrigin(0)
-        .setDepth(2)
-        .setScrollFactor(0);
+        .setDepth(2);
 
-
-
-        // CABLES SUPERIORES
+        this.floor = this.add.image(
+            0,
+            0,
+            "floor"
+        )
+        .setOrigin(0)
+        .setDepth(3);
 
         this.cables = this.add.image(
             0,
@@ -86,228 +77,229 @@ export default class WorldScene extends Phaser.Scene {
             "cables"
         )
         .setOrigin(0)
-        .setDepth(3)
-        .setScrollFactor(0);
-
-
+        .setDepth(5);
 
         //=========================================
-        // PISO VISUAL
+        // TAMAÑO ORIGINAL
         //=========================================
 
+        this.sky.setDisplaySize(
+            1672,
+            941
+        );
 
-        this.floorImage = this.add.image(
-            0,
-            620,
-            "floor"
-        )
-        .setOrigin(0)
-        .setDepth(4);
+        this.cityBack.setDisplaySize(
+            1672,
+            941
+        );
 
+        this.cityFront.setDisplaySize(
+            1672,
+            941
+        );
 
+        this.floor.setDisplaySize(
+            1672,
+            941
+        );
+
+        this.cables.setDisplaySize(
+            1672,
+            941
+        );
 
         //=========================================
-        // PISO FISICO
+        // SUELO FÍSICO
         //=========================================
-
 
         this.ground = this.add.rectangle(
-            1500,
-            690,
-            3000,
-            60,
+            836,
+            FLOOR_Y,
+            1672,
+            24,
             0x000000,
             0
         );
-
 
         this.physics.add.existing(
             this.ground,
             true
         );
 
-
-
         //=========================================
-        // TITULO
+        // ANIMACIONES
         //=========================================
 
-        this.add.text(
-            640,
-            50,
-            "VALLE DEL CUARZO\nDEL ALMA",
-            {
-                fontSize:"32px",
-                color:"#00ffff",
-                fontStyle:"bold",
-                align:"center"
-            }
-        )
-        .setOrigin(0.5)
-        .setScrollFactor(0)
-        .setDepth(10);
-
-
+        createKaelAnimations(this);
 
         //=========================================
         // KAEL
         //=========================================
 
-
-        createKaelAnimations(this);
-
-
         this.kael = this.physics.add.sprite(
-            200,
-            500,
+            250,
+            FLOOR_Y - 80,
             "kael"
         );
 
-
-        this.kael.setDepth(5);
+        this.kael.setDepth(4);
 
         this.kael.setBounce(0);
 
         this.kael.setCollideWorldBounds(true);
-
-
 
         this.physics.add.collider(
             this.kael,
             this.ground
         );
 
-
-
+            //=========================================
+        // CÁMARA
         //=========================================
-        // CAMARA
-        //=========================================
-
 
         this.cameras.main.startFollow(
             this.kael,
-            true
+            true,
+            0.08,
+            0.08
         );
-
-
-        this.cameras.main.setBounds(
-            0,
-            0,
-            3000,
-            720
-        );
-
-
 
         //=========================================
         // CONTROLES
         //=========================================
 
-
         this.cursors =
-        this.input.keyboard.createCursorKeys();
-
-
+            this.input.keyboard.createCursorKeys();
 
         this.keys =
-        this.input.keyboard.addKeys({
+            this.input.keyboard.addKeys({
 
-            A: Phaser.Input.Keyboard.KeyCodes.A,
+                A: Phaser.Input.Keyboard.KeyCodes.A,
 
-            D: Phaser.Input.Keyboard.KeyCodes.D,
+                D: Phaser.Input.Keyboard.KeyCodes.D,
 
-            SPACE: Phaser.Input.Keyboard.KeyCodes.SPACE
+                SPACE: Phaser.Input.Keyboard.KeyCodes.SPACE
 
-        });
+            });
 
+        //=========================================
+        // TÍTULO DEL NIVEL
+        //=========================================
+
+        this.add.text(
+            640,
+            40,
+            "VALLE DEL CUARZO DEL ALMA",
+            {
+                fontSize: "32px",
+                color: "#00ffff",
+                fontStyle: "bold"
+            }
+        )
+        .setOrigin(0.5)
+        .setScrollFactor(0)
+        .setDepth(100);
 
     }
 
-
-
-
     update() {
-
-
 
         //=========================================
         // PARALLAX
         //=========================================
 
+        const scrollX = this.cameras.main.scrollX;
 
-        this.cityBack.x =
-        this.cameras.main.scrollX * 0.15;
+        this.sky.x = scrollX * 0.05;
 
+        this.cityBack.x = scrollX * 0.15;
 
+        this.cityFront.x = scrollX * 0.30;
 
-        this.cityFront.x =
-        this.cameras.main.scrollX * 0.30;
-
-
-
-        this.cables.x =
-        this.cameras.main.scrollX * 0.45;
-
-
-
+        this.cables.x = scrollX * 0.45;
 
         //=========================================
-        // MOVIMIENTO KAEL
+        // MOVIMIENTO
         //=========================================
 
-
-        if(
+        if (
             this.keys.A.isDown ||
             this.cursors.left.isDown
-        ){
+        ) {
 
-            this.kael.setVelocityX(-180);
+            this.kael.setVelocityX(-220);
 
             this.kael.setFlipX(true);
 
-        }
+            if (this.anims.exists("kael-walk")) {
 
-        else if(
+                this.kael.play(
+                    "kael-walk",
+                    true
+                );
+
+            }
+
+        }
+        else if (
             this.keys.D.isDown ||
             this.cursors.right.isDown
-        ){
+        ) {
 
-            this.kael.setVelocityX(180);
+            this.kael.setVelocityX(220);
 
             this.kael.setFlipX(false);
 
-        }
+            if (this.anims.exists("kael-walk")) {
 
-        else{
+                this.kael.play(
+                    "kael-walk",
+                    true
+                );
+
+            }
+
+        }
+        else {
 
             this.kael.setVelocityX(0);
 
+            if (this.anims.exists("kael-idle")) {
+
+                this.kael.play(
+                    "kael-idle",
+                    true
+                );
+
+            }
+
         }
-
-
-
-        //=========================================
+                //=========================================
         // SALTO
         //=========================================
 
-
-        if(
+        if (
             (
                 this.keys.SPACE.isDown ||
                 this.cursors.up.isDown
-            )
-            &&
+            ) &&
             this.kael.body.blocked.down
-        ){
+        ) {
 
-            this.kael.setVelocityY(-500);
+            this.kael.setVelocityY(-520);
+
+            if (this.anims.exists("kael-jump")) {
+
+                this.kael.play(
+                    "kael-jump",
+                    true
+                );
+
+            }
 
         }
 
-
     }
 
-
-} 
-
-      
+}
+  

62b56b5c579760b28c03ebd173cccad294672ce4
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-06 01:11:23 -0500

    Update MenuScene.js

--- game/scenes/MenuScene.js
+++ game/scenes/MenuScene.js
@@ -1,57 +1,50 @@
 export default class MenuScene extends Phaser.Scene {
 
+
     constructor() {
+
         super("MenuScene");
+
     }
+
 
 
     create() {
 
+
         console.log("MenuScene funcionando");
 
-
-        // ==============================
-        // FONDO DEL MENU
-        // ==============================
 
         this.cameras.main.setBackgroundColor("#050505");
 
 
-
-        // ==============================
-        // TITULO
-        // ==============================
 
         this.add.text(
             400,
             180,
             "LOS 7 CUARZOS",
             {
-                fontSize: "48px",
-                fontFamily: "Arial",
-                color: "#00ffff"
+                fontSize:"48px",
+                fontFamily:"Arial",
+                color:"#00ffff"
             }
         )
         .setOrigin(0.5);
 
 
 
-        // ==============================
-        // BOTON INICIO
-        // ==============================
-
         const boton = this.add.text(
             400,
             330,
             "INICIO",
             {
-                fontSize: "35px",
-                fontFamily: "Arial",
-                color: "#ffffff",
-                backgroundColor: "#111111",
-                padding: {
-                    x: 30,
-                    y: 15
+                fontSize:"35px",
+                fontFamily:"Arial",
+                color:"#ffffff",
+                backgroundColor:"#111111",
+                padding:{
+                    x:30,
+                    y:15
                 }
             }
         )
@@ -60,13 +53,9 @@ export default class MenuScene extends Phaser.Scene {
 
 
 
-        // ==============================
-        // EFECTO DEL BOTON
-        // ==============================
-
         boton.on(
             "pointerover",
-            () => {
+            ()=>{
 
                 boton.setStyle({
                     color:"#00ffff"
@@ -76,9 +65,10 @@ export default class MenuScene extends Phaser.Scene {
         );
 
 
+
         boton.on(
             "pointerout",
-            () => {
+            ()=>{
 
                 boton.setStyle({
                     color:"#ffffff"
@@ -89,13 +79,9 @@ export default class MenuScene extends Phaser.Scene {
 
 
 
-        // ==============================
-        // ENTRAR AL MUNDO
-        // ==============================
-
         boton.on(
             "pointerdown",
-            () => {
+            ()=>{
 
                 console.log("Entrando al Valle del Alma");
 
@@ -107,4 +93,6 @@ export default class MenuScene extends Phaser.Scene {
 
     }
 
+
 }
+  

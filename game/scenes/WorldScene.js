d0bcfe2fb0d5f12d7f7eaff6b3f517ec6602773d
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-06 00:45:30 -0500

    Update WorldScene.js

--- game/scenes/WorldScene.js
+++ game/scenes/WorldScene.js
@@ -1,56 +1,222 @@
-export default class PreloadScene extends Phaser.Scene {
+export default class WorldScene extends Phaser.Scene {
+
 
     constructor() {
-        super("PreloadScene");
-    }
 
-
-    preload() {
-
-
-        // ==============================
-        // VALLE DEL CUARZO DEL ALMA
-        // ==============================
-
-        this.load.image(
-            "sky",
-            "game/assets/valle del_alma/sky.png"
-        );
-
-
-        this.load.image(
-            "city_back",
-            "game/assets/valle del_alma/city_back.png"
-        );
-
-
-        this.load.image(
-            "city_front",
-            "game/assets/valle del_alma/city_front.png"
-        );
-
-
-        this.load.image(
-            "cables",
-            "game/assets/valle del_alma/cables.png"
-        );
-
-
-        this.load.image(
-            "floor",
-            "game/assets/valle del_alma/floor.png"
-        );
-
+        super("WorldScene");
 
     }
 
 
     create() {
 
-        console.log("Cargando Valle del Alma...");
 
-        this.scene.start("MenuScene");
+        // ==================================
+        // PARALLAX DEL VALLE
+        // ==================================
+
+
+        this.add.tileSprite(
+            0,
+            0,
+            1280,
+            720,
+            "sky"
+        )
+        .setOrigin(0)
+        .setScrollFactor(0);
+
+
+
+        this.add.tileSprite(
+            0,
+            0,
+            3000,
+            720,
+            "city_back"
+        )
+        .setOrigin(0)
+        .setScrollFactor(0.2);
+
+
+
+        this.add.tileSprite(
+            0,
+            0,
+            3000,
+            720,
+            "city_front"
+        )
+        .setOrigin(0)
+        .setScrollFactor(0.5);
+
+
+
+        this.add.tileSprite(
+            0,
+            0,
+            3000,
+            720,
+            "cables"
+        )
+        .setOrigin(0)
+        .setScrollFactor(0.7);
+
+
+
+        // ==================================
+        // TITULO
+        // ==================================
+
+        this.add.text(
+            640,
+            40,
+            "VALLE DEL CUARZO DEL ALMA",
+            {
+                fontSize:"32px",
+                color:"#00ffff"
+            }
+        )
+        .setOrigin(0.5)
+        .setScrollFactor(0);
+
+
+
+        // ==================================
+        // LIMITES DEL MUNDO
+        // ==================================
+
+        this.physics.world.setBounds(
+            0,
+            0,
+            3000,
+            720
+        );
+
+
+
+        // ==================================
+        // PISO
+        // ==================================
+
+        this.floor = this.physics.add.staticImage(
+            1500,
+            680,
+            "floor"
+        );
+
+
+        this.floor.setDisplaySize(
+            3000,
+            80
+        );
+
+
+        this.floor.refreshBody();
+
+
+
+        // ==================================
+        // KAEL CUADRO DE PRUEBA
+        // ==================================
+
+        this.kael = this.add.rectangle(
+            200,
+            500,
+            40,
+            60,
+            0x00ff00
+        );
+
+
+        this.physics.add.existing(
+            this.kael
+        );
+
+
+        this.kael.body.setCollideWorldBounds(true);
+
+
+
+        // COLISION CON PISO
+
+        this.physics.add.collider(
+            this.kael,
+            this.floor
+        );
+
+
+
+        // ==================================
+        // CAMARA
+        // ==================================
+
+        this.cameras.main.startFollow(
+            this.kael
+        );
+
+
+        this.cameras.main.setBounds(
+            0,
+            0,
+            3000,
+            720
+        );
+
+
+
+        // ==================================
+        // CONTROLES
+        // ==================================
+
+        this.keys = this.input.keyboard.createCursorKeys();
+
 
     }
 
-}
+
+
+    update(){
+
+
+
+        // MOVIMIENTO
+
+        if(this.keys.left.isDown){
+
+            this.kael.body.setVelocityX(-250);
+
+        }
+
+
+        else if(this.keys.right.isDown){
+
+            this.kael.body.setVelocityX(250);
+
+        }
+
+
+        else{
+
+            this.kael.body.setVelocityX(0);
+
+        }
+
+
+
+        // SALTO
+
+        if(
+            this.keys.up.isDown &&
+            this.kael.body.touching.down
+        ){
+
+            this.kael.body.setVelocityY(-500);
+
+        }
+
+
+    }
+
+
+

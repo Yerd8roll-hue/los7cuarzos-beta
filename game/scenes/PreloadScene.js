4fd5c406495f8c739e42ba0161e389a78f037afc
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-06 15:33:58 -0500

    Update PreloadScene.js

--- game/scenes/PreloadScene.js
+++ game/scenes/PreloadScene.js
@@ -4,50 +4,43 @@ export default class PreloadScene extends Phaser.Scene {
         super("PreloadScene");
     }
 
-
     preload() {
 
-
         console.log("Cargando recursos...");
-
 
         // ==============================
         // VALLE DEL CUARZO DEL ALMA
         // ==============================
-
 
         this.load.image(
             "sky",
             "game/assets/valle del_alma/sky.png"
         );
 
-
-        this.load.image(
-            "city_back",
-            "game/assets/valle del_alma/city_back.png"
-        );
-
-
-        this.load.image(
-            "city_front",
-            "game/assets/valle del_alma/city_front.png"
-        );
-
-
         this.load.image(
             "cables",
             "game/assets/valle del_alma/cables.png"
         );
-
 
         this.load.image(
             "floor",
             "game/assets/valle del_alma/floor.png"
         );
 
+        // ==============================
+        // KAEL
+        // ==============================
+
+        this.load.spritesheet(
+            "kael",
+            "game/assets/personajes/Kael/spritesheet.png",
+            {
+                frameWidth: 64,
+                frameHeight: 64
+            }
+        );
 
     }
-
 
     create() {
 

3b69c9019aac81a11adcbfe5de9f9ca29232b418
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-06 00:33:51 -0500

    Update PreloadScene.js

--- game/scenes/PreloadScene.js
+++ game/scenes/PreloadScene.js
@@ -1,125 +1,65 @@
 export default class PreloadScene extends Phaser.Scene {
 
-
     constructor() {
-
         super("PreloadScene");
-
     }
-
-
 
     preload() {
 
-
-
-        //=========================================
-        // KAEL
-        //=========================================
-
+        // ==============================
+        // KAEL SPRITESHEET
+        // ==============================
 
         this.load.spritesheet(
-
             "kael",
-
             "game/assets/personajes/Kael/spritesheet.png",
-
             {
                 frameWidth: 64,
                 frameHeight: 64
             }
-
         );
 
 
-
-
-        //=========================================
+        // ==============================
         // VALLE DEL CUARZO DEL ALMA
-        //=========================================
-
+        // ==============================
 
         this.load.image(
-
             "sky",
-
-            "game/assets/valle%20del_alma/sky.png"
-
+            "game/assets/valle del_alma/sky.png"
         );
 
-
-
         this.load.image(
-
-            "cityBack",
-
-            "game/assets/valle%20del_alma/city_back.png"
-
+            "city_back",
+            "game/assets/valle del_alma/city_back.png"
         );
 
-
-
         this.load.image(
-
-            "cityFront",
-
-            "game/assets/valle%20del_alma/city_front.png"
-
+            "city_front",
+            "game/assets/valle del_alma/city_front.png"
         );
 
-
-
         this.load.image(
-
             "cables",
-
-            "game/assets/valle%20del_alma/cables.png"
-
+            "game/assets/valle del_alma/cables.png"
         );
 
-
-
         this.load.image(
-
             "floor",
-
-            "game/assets/valle%20del_alma/floor.png"
-
+            "game/assets/valle del_alma/floor.png"
         );
-
-
-
-
-        //=========================================
-        // CUARZO ALMA
-        //=========================================
-
-
-        this.load.image(
-
-            "cuarzo-alma",
-
-            "game/assets/cuarzos/alma.png"
-
-        );
-
-
 
     }
-
-
 
 
     create() {
 
+        console.log("Assets cargados correctamente");
 
-        console.log("Preload listo");
-
-
-        this.scene.start("WorldScene");
-
+        this.scene.start("MenuScene");
 
     }
 
-
 }
+
+ 

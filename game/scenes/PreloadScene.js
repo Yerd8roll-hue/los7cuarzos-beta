d301c9b3ebe7bd693c537640b94089ccca3b6f04
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-06 00:40:38 -0500

    Update PreloadScene.js

--- game/scenes/PreloadScene.js
+++ game/scenes/PreloadScene.js
@@ -4,20 +4,8 @@ export default class PreloadScene extends Phaser.Scene {
         super("PreloadScene");
     }
 
+
     preload() {
-
-        // ==============================
-        // KAEL SPRITESHEET
-        // ==============================
-
-        this.load.spritesheet(
-            "kael",
-            "game/assets/personajes/Kael/spritesheet.png",
-            {
-                frameWidth: 64,
-                frameHeight: 64
-            }
-        );
 
 
         // ==============================
@@ -29,37 +17,40 @@ export default class PreloadScene extends Phaser.Scene {
             "game/assets/valle del_alma/sky.png"
         );
 
+
         this.load.image(
             "city_back",
             "game/assets/valle del_alma/city_back.png"
         );
+
 
         this.load.image(
             "city_front",
             "game/assets/valle del_alma/city_front.png"
         );
 
+
         this.load.image(
             "cables",
             "game/assets/valle del_alma/cables.png"
         );
+
 
         this.load.image(
             "floor",
             "game/assets/valle del_alma/floor.png"
         );
 
+
     }
 
 
     create() {
 
-        console.log("Assets cargados correctamente");
+        console.log("Cargando Valle del Alma...");
 
         this.scene.start("MenuScene");
 
     }
 
 }
-
- 

cdb43032a0f7479c9ab1d074a9bdcf270510736a
LOS7CUARZOS <Yerd8_roll@hotmail.com>
2026-08-08 18:34:13 -0500

    Update WorldScene.js

--- game/scenes/WorldScene.js
+++ game/scenes/WorldScene.js
@@ -1,3 +1,4 @@
+```js
 import { createKaelAnimations } from "../animations/KaelAnimations.js";
 
 export default class WorldScene extends Phaser.Scene {
@@ -6,93 +7,35 @@ export default class WorldScene extends Phaser.Scene {
         super("WorldScene");
     }
 
-
     create() {
-
 
         const mundoAncho = 3000;
 
-
-        // ==================================
         // FONDO
-        // ==================================
-
         this.cameras.main.setBackgroundColor("#05070d");
 
-
-        // ==================================
         // MUNDO
-        // ==================================
+        this.physics.world.setBounds(0, 0, mundoAncho, 720);
 
-        this.physics.world.setBounds(
-            0,
-            0,
-            mundoAncho,
-            720
-        );
-
-
-        // ==================================
         // SKY
-        // ==================================
+        this.sky = this.add.image(0, 0, "sky");
+        this.sky.setOrigin(0, 0);
+        this.sky.setDepth(0);
+        this.sky.setScrollFactor(0);
 
-        this.sky = this.add.image(
-            0,
-            0,
-            "sky"
-        );
-
-        this.sky
-            .setOrigin(0)
-            .setDepth(0)
-            .setScrollFactor(0);
-
-
-
-        // ==================================
         // CABLES
-        // ==================================
+        this.cables = this.add.image(0, 250, "cables");
+        this.cables.setOrigin(0, 0);
+        this.cables.setDisplaySize(mundoAncho, 180);
+        this.cables.setDepth(2);
 
-        this.cables = this.add.image(
-            0,
-            250,
-            "cables"
-        );
+        // FLOOR
+        this.floorImage = this.add.image(0, 570, "floor");
+        this.floorImage.setOrigin(0, 0);
+        this.floorImage.setDisplaySize(mundoAncho, 150);
+        this.floorImage.setDepth(3);
 
-        this.cables
-            .setOrigin(0)
-            .setDisplaySize(
-                mundoAncho,
-                180
-            )
-            .setDepth(2);
-
-
-
-        // ==================================
-        // FLOOR VISUAL
-        // ==================================
-
-        this.floorImage = this.add.image(
-            0,
-            570,
-            "floor"
-        );
-
-        this.floorImage
-            .setOrigin(0)
-            .setDisplaySize(
-                mundoAncho,
-                150
-            )
-            .setDepth(3);
-
-
-
-        // ==================================
         // PISO FISICO
-        // ==================================
-
         this.ground = this.add.rectangle(
             mundoAncho / 2,
             650,
@@ -102,20 +45,10 @@ export default class WorldScene extends Phaser.Scene {
             0
         );
 
+        this.physics.add.existing(this.ground, true);
 
-        this.physics.add.existing(
-            this.ground,
-            true
-        );
-
-
-
-        // ==================================
         // KAEL
-        // ==================================
-
         createKaelAnimations(this);
-
 
         this.kael = this.physics.add.sprite(
             200,
@@ -123,29 +56,19 @@ export default class WorldScene extends Phaser.Scene {
             "kael"
         );
 
-
         this.kael.setDepth(5);
-
         this.kael.setCollideWorldBounds(true);
-
-
 
         this.physics.add.collider(
             this.kael,
             this.ground
         );
 
-
-
-        // ==================================
         // CAMARA
-        // ==================================
-
         this.cameras.main.startFollow(
             this.kael,
             true
         );
-
 
         this.cameras.main.setBounds(
             0,
@@ -154,84 +77,63 @@ export default class WorldScene extends Phaser.Scene {
             720
         );
 
-
-
-        // ==================================
         // CONTROLES
-        // ==================================
-
         this.cursors =
             this.input.keyboard.createCursorKeys();
 
-
         this.keys =
             this.input.keyboard.addKeys({
-
                 A: Phaser.Input.Keyboard.KeyCodes.A,
-
                 D: Phaser.Input.Keyboard.KeyCodes.D,
-
                 SPACE: Phaser.Input.Keyboard.KeyCodes.SPACE
-
             });
-
-
     }
-
-
 
     update() {
 
-
-        if(
+        // MOVIMIENTO IZQUIERDA
+        if (
             this.keys.A.isDown ||
             this.cursors.left.isDown
-        ){
+        ) {
 
             this.kael.setVelocityX(-180);
 
         }
 
-
-        else if(
+        // MOVIMIENTO DERECHA
+        else if (
             this.keys.D.isDown ||
             this.cursors.right.isDown
-        ){
+        ) {
 
             this.kael.setVelocityX(180);
 
         }
 
-
-        else{
+        // QUIETO
+        else {
 
             this.kael.setVelocityX(0);
 
         }
 
-
-
-        if(
+        // SALTO
+        if (
             (
                 this.keys.SPACE.isDown ||
                 this.cursors.up.isDown
-            )
-            &&
+            ) &&
             this.kael.body.blocked.down
-        ){
+        ) {
 
             this.kael.setVelocityY(-500);
-
         }
-
-
     }
-
 }
 
 
 
-    
 
- 
+
 
